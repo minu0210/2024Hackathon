c41dff8a73d93a71814404af56f39439f80feeb2
minu0210 <113428736+minu0210@users.noreply.github.com>
2024-01-11 12:28:06 +0900

    Feat: Add index

--- src/index.js
+++ src/index.js
@@ -1,13 +1,54 @@
-import express from 'express';
-import dotenv from 'dotenv';
-dotenv.config();
+import _ from 'lodash'
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+import { connectDB } from './db/connectDB.js'
+import { initExpressApp } from './server/initExpressApp.js'
 
-const app = express();
+import { getUsersRoute } from './server/routes/users/getUsers.js'
+import { signupRoute } from './server/routes/users/signup.js'
+import { signinRoute } from './server/routes/users/signin.js'
+import { createPostRoute } from './server/routes/posts/createPost.js'
+import { getPostsRoute } from './server/routes/posts/getPosts.js'
 
-app.get('/', (req, res) => {
-    res.send("안녕하세요")
-})
+const routes = [
+    getUsersRoute,
+    signupRoute,
+    signinRoute,
+    createPostRoute,
+    getPostsRoute
+]
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on http://localhost:${process.env.PORT}`);
-});
+async function bootstrap () {
+    const app = express()
+    await connectDB()
+
+    await initExpressApp(app)
+
+    routes.forEach(route => {
+        app[route.method](route.path, (req, res) => {
+            route.handler(req, res)
+            .catch((err) => {
+                console.error('Api Error:', err)
+
+                const [statusCode, errorMessage] = err.message.split(':')
+                
+                return res
+                    .status(statusCode)
+                    .json({
+                        success: false,
+                        message: errorMessage
+                })
+            })
+        })
+    })
+
+    app.listen(process.env.PORT, () => {
+        console.log(`App is running on http://localhost:${process.env.PORT}`);
+    })
+}
+
+bootstrap()
+    .catch(err => {
+        console.error(err)
+    })
